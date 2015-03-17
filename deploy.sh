b2d01e6ab77fb76be9b3c333396b6c58f7196527
byjo <buyongjo@gmail.com>
2015-03-17 12:55:10 +0900

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git pull
+git pull origin master
 
 mvn clean package
 
 /Users/JoJo/Downloads/NHNNEXT/2015_01/hudi/hudi-workspace/apache-tomcat-8.0.20/bin/shutdown.sh
 
-rsync -avr --delete /Users/JoJo/Downloads/NHNNEXT/2015_01/hudi/hudi-workspace/myproject/target/myproject /Users/JoJo/Downloads/NHNNEXT/2015_01/hudi/hudi-workspace/apache-tomcat-8.0.14/webapps
+rsync -avr --delete /Users/JoJo/Downloads/NHNNEXT/2015_01/hudi/hudi-workspace/myproject/target/myproject /Users/JoJo/Downloads/NHNNEXT/2015_01/hudi/hudi-workspace/apache-tomcat-8.0.20/webapps
 
 /Users/JoJo/Downloads/NHNNEXT/2015_01/hudi/hudi-workspace/apache-tomcat-8.0.20/bin/startup.sh
